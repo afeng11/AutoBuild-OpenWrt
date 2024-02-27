4d88d9741c2178444765c1dc19e87d720f9605dd
xuzhun <38719045+afeng11@users.noreply.github.com>
2024-02-27 11:33:34 +0800

    Update diy-part5.sh

--- diy-part5.sh
+++ diy-part5.sh
@@ -11,16 +11,9 @@
 #=================================================
 
 # 修改默认IP
-sed -i 's/192.168.1.1/10.32.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 # 修改网关
-sed -i 's/192.168.$((addr_offset++)).1/10.32.$((addr_offset++)).1/g' package/base-files/files/bin/config_generate
-
-# 修改主机名称
-sed -i 's/OpenWrt/Yuos/g' package/base-files/files/bin/config_generate
-
-# 修改版本号
-# sed -i "s/OpenWrt/小渔学长 build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/default-settings/files/zzz-default-settings
-
+sed -i 's/192.168.$((addr_offset++)).1/192.168.$((addr_offset++)).1/g' package/base-files/files/bin/config_generate
 
 # Add kernel build user
 [ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
@@ -37,22 +30,3 @@ wget -O package/firmware/xt_FULLCONENAT.c https://raw.githubusercontent.com/Chio
 cp -rf package/firmware/xt_FULLCONENAT.c package/nftables/include/linux/netfilter/xt_FULLCONENAT.c
 cp -rf package/firmware/xt_FULLCONENAT.c package/libnftnl/include/linux/netfilter/xt_FULLCONENAT.c
 cp -rf package/firmware/xt_FULLCONENAT.c package/libs/libnetfilter-conntrack/xt_FULLCONENAT.c
-
-# dnsmasq-full升级2.89
-rm -rf package/network/services/dnsmasq
-cp -rf $GITHUB_WORKSPACE/patchs/5.4/dnsmasq package/network/services/dnsmasq
-
-# 补丁
-rm -rf package/network/utils
-cp -rf $GITHUB_WORKSPACE/patchs/5.4/network/utils package/network/utils
-
-# 测试编译时间
-YUOS_DATE="$(date +%Y.%m.%d)(自用版)"
-BUILD_STRING=${BUILD_STRING:-$YUOS_DATE}
-echo "Write build date in openwrt : $BUILD_DATE"
-echo -e '\n小渔学长 Build @ '${BUILD_STRING}'\n'  >> package/base-files/files/etc/banner
-sed -i '/DISTRIB_REVISION/d' package/base-files/files/etc/openwrt_release
-echo "DISTRIB_REVISION=''" >> package/base-files/files/etc/openwrt_release
-sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
-echo "DISTRIB_DESCRIPTION='小渔学长 Build @ ${BUILD_STRING}'" >> package/base-files/files/etc/openwrt_release
-sed -i '/luciversion/d' feeds/luci/modules/luci-base/luasrc/version.lua
