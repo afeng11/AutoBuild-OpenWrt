0fe8a890155507ef48b471d3602e9d50d139f5b8
xuzhun <38719045+afeng11@users.noreply.github.com>
2024-03-16 03:33:43 +0800

    Update diy-part5.sh

--- diy-part5.sh
+++ diy-part5.sh
@@ -11,7 +11,7 @@
 #=================================================
 
 # 修改默认IP
-# sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 # 修改网关
 # sed -i 's/192.168.$((addr_offset++)).1/192.168.$((addr_offset++)).1/g' package/base-files/files/bin/config_generate
 
