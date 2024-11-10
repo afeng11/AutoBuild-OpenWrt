3ecb04003729182a7dd3d0c1b4e96005ccb8db38
xuzhun <38719045+afeng11@users.noreply.github.com>
2024-11-10 11:00:13 +0800

    Update diy-part5.sh

--- diy-part5.sh
+++ diy-part5.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 # 修改网关
 #sed -i 's/192.168.$((addr_offset++)).1/192.168.$((addr_offset++)).1/g' package/base-files/files/bin/config_generate
-# 修改默认wifi名称ssid为Zte_1
-sed -i 's/ssid=OpenWrt/ssid=Zte_${devidx}/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# 修改默认wifi名称ssid为Zte_E8820-v1
+sed -i 's/ssid=OpenWrt/ssid=Zte_E8820v1_${devidx}/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #修改默认wifi开启
 sed -i 's/wireless.radio${devidx}.disabled=1/wireless.radio${devidx}.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
