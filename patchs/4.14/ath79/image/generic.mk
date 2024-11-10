2123448c5bffc2e89320c33c639ca963c4255b7a
xuzhun <38719045+afeng11@users.noreply.github.com>
2024-11-10 09:21:02 +0800

    Update generic.mk

--- patchs/4.14/ath79/image/generic.mk
+++ patchs/4.14/ath79/image/generic.mk
@@ -674,13 +674,14 @@ define Device/rosinson_wr818
 endef
 TARGET_DEVICES += rosinson_wr818
 
-define Device/zte_e8820v1
+define Device/zte_zte-e8820-v1
   ATH_SOC := qca9563
-  DEVICE_TITLE := ZTE E8820V1
-  IMAGE_SIZE := 15872k
-  DEVICE_PACKAGES := kmod-usb-core kmod-usb2 kmod-usb-ledtrig-usbport kmod-usb3
+  DEVICE_TITLE := Zte E8820 v1
+  IMAGE_SIZE := 16000k
+  DEVICE_PACKAGES := kmod-usb-core kmod-usb2 kmod-usb-ledtrig-usbport kmod-usb3 ath10k-firmware-qca988x-ct
+  SUPPORTED_DEVICES += zte-e8820-v1
 endef
-TARGET_DEVICES += zte_e8820v1
+TARGET_DEVICES += zte_zte-e8820-v1
 
 define Device/wd_mynet-n750
   $(Device/seama)
